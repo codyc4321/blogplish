8efea851dbd9a829b2700b44108c49630fcb39d2
Cody Childers <cchilder@mail.usf.edu>
2017-07-29 23:49:41 -0500

    As far as the first step "Write function to call Bash command from Python" goes, in theory, I already had a sturdy function to flexibly run linux commands in a python script:

--- blogplish.py
+++ blogplish.py
@@ -1,7 +1,7 @@
 
+from subprocess import Popen, PIPE
 
 
-print("The script is working.")
 
 """
 Write function to call Bash command from Python
@@ -13,3 +13,19 @@ For each commit in the commit info:
     Add changes to final string
     Add entire files that were changed to final string
 """
+
+
+def call_sp(command, *args, **kwargs):
+    """ you can run command from any directory you want by passing in a kwarg of 'cwd' (current working directory):
+
+        call_sp('ls', '-a', cwd='/home/username/projects/awesomeproject')
+    """
+    if args:
+        command = command.format(*args)
+    p = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, **kwargs)
+    output, err = p.communicate()
+    return output, err
+
+
+output, error = call_sp('ls')
+print(output)
