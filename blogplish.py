536ec976da6b09a0c9aa08086169081bad474c8e
Cody Childers <cchilder@mail.usf.edu>
2017-07-29 23:28:47 -0500

    When you're working on a task you've never done before, most of your time is usually spent figuring out what it is you need to do.
    In this case, we found ourselves stuck planning out how to write the script (Do we use regular Bash, or the Github API?, Do we get all commits at once, or go backwards using the HEAD~1 style syntax until there's no commits left?, and so on).
    When you get that stuck, it's best to start out writing pseudocode and describe what you think you need to do overall:

--- blogplish.py
+++ blogplish.py
@@ -2,4 +2,14 @@
 
 
 print("The script is working.")
-x
+
+"""
+Write function to call Bash command from Python
+
+Get all commit info
+
+For each commit in the commit info:
+    Add commit message to a final string
+    Add changes to final string
+    Add entire files that were changed to final string
+"""
